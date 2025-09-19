aff033c192002a2639a4924a8abf729ef36d36ad
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2025-09-19 17:18:36 +0200

    add piglatin

--- src/games/pigLatin.ts
+++ src/games/pigLatin.ts
@@ -8,5 +8,31 @@ export function playPigLatin(state: State) {
         if (!userInput) {
             rl.prompt();
         }
+        const encodedSentence = translateSentence(userInput);
+        console.log(encodedSentence);
+        process.exit();
     });
+}
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+function mutateWord(word: string) {
+    const firstChar = word[0].toLowerCase();
+    const secondChar = word[1].toLowerCase();
+
+    if (vowels.includes(firstChar)) {
+        return word + 'way';
+    }
+
+    if (!vowels.includes(firstChar) && vowels.includes(secondChar)) {
+        return word.slice(1) + firstChar + 'ay';
+    }
+
+    return word.slice(2) + firstChar + secondChar + 'ay';
+}
+
+function translateSentence(sentence: string) {
+    const sentenceArray = sentence.split(' ');
+    const translation = sentenceArray.map((word) => mutateWord(word));
+    return translation.join(' ');
 }
